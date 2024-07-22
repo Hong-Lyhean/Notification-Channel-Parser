66fe0a0ecd7d9eb5a189b9f7f192f89a7318c868
Hong-Lyhean <lyhean23@gmail.com>
2024-07-22 15:51:40 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        string[] channel = {"BE", "FE", "QA", "Urgent"};
+        readonly string pattern = @"\[(.*?)\]";
+
+        static void Main(string[] args)
+        {
+            Program department = new Program();
+            Console.WriteLine("Enter a Notification Title:");
+            string title = Console.ReadLine() ?? "";
+            // string title = "[BE][HE][Urgent][BE] there is error";
+
+            Console.WriteLine(ParseTitle(title, department.pattern, department.channel));
+        }
+
+        static string ParseTitle(string title, string pattern, string[] channels){
+            MatchCollection matches = Regex.Matches(title, pattern);
+            List<string> receiveChannel = [];
+
+            foreach (Match match in matches)
+            {
+                string valueInsideBrackets = match.Groups[1].Value;
+                bool is_exist = Array.Exists(channels, channel => channel == valueInsideBrackets);
+                bool is_duplicate = receiveChannel.Contains(valueInsideBrackets);
+                if(is_exist == true && is_duplicate == false){
+                    receiveChannel.Add(valueInsideBrackets);
+                }
+            }
+            return "Receive channels: " + string.Join(", ", receiveChannel);
+        }
+    }
+}
